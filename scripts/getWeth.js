69aba0e5de45781bee05347168f901a3e0623037
Owanikin <oderindeife@gmail.com>
2022-09-28 09:03:26 +0100

    depositing to aave

--- scripts/getWeth.js
+++ scripts/getWeth.js
@@ -1,25 +1,3 @@
-// const { getNamedAccounts, ethers } = require("hardhat")
-
-// const AMOUNT = ethers.utils.parseEther("0.02")
-
-// async function getWeth() {
-//     const { deployer } = getNamedAccounts()
-//     // call the "deposit" function on the weth contract
-//     // We'll need the abi, & contract address
-//     // 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2
-//     const iWeth = await ethers.getContractAt(
-//         "IWeth",
-//         "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
-//         deployer
-//     )
-//     const tx = await iWeth.deposit({ value: AMOUNT })
-//     await tx.wait(1)
-//     const wethBalance = await iWeth.balanceOf(deployer)
-//     console.log(`Got ${wethBalance.toString()} WETH`)
-// }
-
-// module.exports = { getWeth, AMOUNT }
-
 const { getNamedAccounts, ethers } = require("hardhat")
 
 const AMOUNT = ethers.utils.parseEther("0.02")
@@ -38,4 +16,4 @@ async function getWeth() {
    console.log(`Got ${wethBalance.toString()} WETH`)
 }
 
-module.exports = { getWeth }
+module.exports = { getWeth, AMOUNT }
